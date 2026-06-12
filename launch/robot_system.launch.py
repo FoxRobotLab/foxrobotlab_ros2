ccd472eaaee63112cfce3f7167daef14c437f3d4
Aissandre <aisumohee@gmail.com>
2026-06-12 10:10:31 -0500

    fixed central launch arguments

--- launch/robot_system.launch.py
+++ launch/robot_system.launch.py
@@ -8,11 +8,12 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
-def _include(package_dir, launch_file):
+def _include(package_dir, launch_file, launch_arguments=None):
     return IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(package_dir, 'launch', launch_file)
-        )
+        ),
+        launch_arguments=(launch_arguments or {}).items(),
     )
 
 
@@ -20,7 +21,7 @@ def generate_launch_description():
     package_dir = get_package_share_directory('foxrobotlab_ros2')
 
     ld = LaunchDescription()
-    ld.add_action(_include(package_dir, 'kobuki.launch.py'))
+    ld.add_action(_include(package_dir, 'kobuki.launch.py', {'astra': 'true'}))
     ld.add_action(_include(package_dir, 'video_stream_client.launch.py'))
     ld.add_action(_include(package_dir, 'match_planner.launch.py'))
     return ld
