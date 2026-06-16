3ec75fd99344852843c0ad2050815568bd2775c9
oscarrezab <110639308+oscarrezab@users.noreply.github.com>
2026-06-16 11:31:20 -0500

    tf keras test

--- src/match_seeker/scripts/tfTest.py
+++ src/match_seeker/scripts/tfTest.py
@@ -4,18 +4,18 @@
 
 import time
 import tensorflow as tf
-#import tf_keras
+import tf_keras
 
 print("Starting...")
 
-#time.sleep(2)
+time.sleep(2)
 
-#print("ANother print statement")
+print("ANother print statement")
 
-#checkptPath = "/media/macalester/Data/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/DATA/CHECKPOINTS"
-#testModelPath = "2024CellPredictLSTM_checkpoint-0802241319/CellPredAdam224-74-1.26.keras"
-#model = tf_keras.models.load_model(checkptPath + "/" + testModelPath)
-#model.save('newCellPredAdam224-74-1.26.keras')
+checkptPath = "/media/macalester/Data/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/DATA/CHECKPOINTS"
+testModelPath = "2024CellPredictLSTM_checkpoint-0802241319/CellPredAdam224-74-1.26.keras"
+model = tf_keras.models.load_model(checkptPath + "/" + testModelPath)
+model.save('newCellPredAdam224-74-1.26.keras')
 
 print(tf.__version__)
 
